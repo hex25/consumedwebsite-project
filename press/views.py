cc795998b3fb42ac52e60f91972ceaa3c6e70329
Hex25 <natasha.minin@gmail.com>
2019-01-19 22:36:38 +0100

    sorted by release added everywhere

--- press/views.py
+++ press/views.py
@@ -5,5 +5,5 @@ from .models import Press
 # Create your views here.
 
 def press(request):
-	press_clippings = Press.objects
+	press_clippings = Press.objects.order_by('-catal_number')
 	return render(request, 'press/press.html',{'press':press_clippings})
